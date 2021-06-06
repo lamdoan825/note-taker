a2eba955d022be4abbfaedf15622253b3db41c68
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-06-06 18:07:55 -0500

    api routes cleaned up and attempted to add delete function

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -3,17 +3,24 @@ const router = require('express').Router();
 
 const saveData = require('../db/saveData');
 
-router.get("/notes", function (req, res) {
-  save
+router.get("/notes", (req, res) => {
+  saveData
     .getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
 
 router.post("/notes", (req, res) => {
-  save
+  saveData
     .addNote(req.body)
     .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", (req, res) => {
+  saveData
+    .removeNote(req.params.id)
+    .then((note) => res.json({ ok: true }))
     .catch(err => res.status(500).json(err));
 });
 
