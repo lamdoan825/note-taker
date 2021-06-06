b80387ba6ba32881f2ce2e8c25f5ef06f83c1c0e
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-06-05 20:45:32 -0500

    starting save functionality

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3001;
+// Create an express server
 const app = express();
+
+// Set PORT
+const PORT = process.env.PORT || 3001;
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
 // parse incoming JSON data
 app.use(express.json());
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
