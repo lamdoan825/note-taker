8f7f158d7586ac9f9c191319900421ae7d273c35
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-06-06 19:13:14 -0500

    fixed uuid error, but function is not working properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,21 @@ const router = require('express').Router();
 
 const saveData = require('../db/saveData');
 
-router.get("/notes", (req, res) => {
+router.get('/notes', (req, res) => {
   saveData
     .getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
   saveData
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch(err => res.status(500).json(err));
 });
 
-router.delete("/notes/:id", (req, res) => {
+router.delete('/notes/:id', (req, res) => {
   saveData
     .removeNote(req.params.id)
     .then((note) => res.json({ ok: true }))
